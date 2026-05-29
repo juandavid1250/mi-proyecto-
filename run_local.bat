85bcc00affcea2acdb752554ccd2b6bb2242cc88
juandavid1250-ship-it <juandavid886@gmail.comgit config --global user.name juandavid1250git config --global user.email juandavid886@gmail.com>
2026-05-28 22:32:40 -0500

    Eliminar carpetas y archivos innecesarios para despliegue

--- run_local.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Ejecuta la aplicación FastAPI desde el entorno virtual local
-cd /d "%~dp0"
-if exist ".venv\Scripts\python.exe" (
-    ".venv\Scripts\python.exe" main.py
-) else (
-    python main.py
-)
